380900769b6a0e40d7b632064f832c8950fb69a7
Harshu021 <dubeyharshu0921@gmail.com>
2023-12-26 14:40:47 +0530

    26-12-2023

---
+++ Script/font.js
@@ -0,0 +1,18 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        mitr: ['Mitr', 'sans-serif'],
+      },
+    },
+  },
+};
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        archivo: ['Archivo', 'sans-serif'],
+      },
+    },
+  },
+};
